36c212816f5638b7dda2fc2093fb42846ad040a0
mawa17 <163126157+mawa17@users.noreply.github.com>
2025-12-01 11:53:38 +0100

    Ideer til (Bastian & Denix)

--- Backend/Services/AccountService.cs
+++ Backend/Services/AccountService.cs
@@ -4,7 +4,19 @@ namespace Backend.Services;
 
 public interface IAccountService
 {
+    /*
+    Indsæt penge
+    Hæv penge
+    Overfør penge
+    Overblik over alle transaktioner unikt til AccountModel
 
+    Rente system? med lån?
+
+    Admin 
+    Se alles transaktioner CRUD
+    Stjæle penge?
+    Ændre på settings?  Rente system? med lån?
+    */
 }
 
 public class AccountService(IDbService dbService, IIdentityService identityService) : IAccountService
