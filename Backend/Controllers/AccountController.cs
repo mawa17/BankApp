8e0e9a50b1cd7a5d86651a7ceaaacb5a21db6e06
mawa17 <163126157+mawa17@users.noreply.github.com>
2025-12-01 11:34:50 +0100

    Init Service & Controller (Bastian & Denix)

---
+++ Backend/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using Backend.Data;
+using Backend.Models;
+using Backend.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Controllers;
+
+
+[ApiController]
+[Route("api/[controller]/[action]")]
+public class AccountController(IAccountService service) : ControllerBase
+{
+
+}
---
+++ Backend/Services/AccountService.cs
@@ -0,0 +1,18 @@
+﻿using Backend.Models;
+
+namespace Backend.Services;
+
+public interface IAccountService
+{
+
+}
+
+public class AccountService(IDbService dbService, IIdentityService identityService) : IAccountService
+{
+    async Task ForeginKeyLookupExample()
+    {
+        var identity = await identityService.FindIdentityAsync("bob");
+        if (identity is null) return;
+        var account = dbService.Query<AccountModel>().FirstOrDefault(x => x.UserId == identity.Id);
+    }
+}
