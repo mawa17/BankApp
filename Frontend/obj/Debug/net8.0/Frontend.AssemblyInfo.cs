9edfd0d330874a1bae1df6331cd9d0f31018efa1
Rasmus Djurhuus <Rasmusdk14@hotmail.com>
2025-12-05 09:17:50 +0100

    Hej

--- Frontend/obj/Debug/net8.0/Frontend.AssemblyInfo.cs
+++ Frontend/obj/Debug/net8.0/Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+184acec6fd21a3ff09e1a21562f3602a0eba6114")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+611d2def9868c51e70c7b08f95a1936769f21229")]
 [assembly: System.Reflection.AssemblyProductAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
